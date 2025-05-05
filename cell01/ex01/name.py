70b8ced74cdeeff174d90efac3fe67960013266b
Varaporn Sukmuang <vsukmuan@z2t14c4.42bangkok.com>
2025-05-05 16:07:13 +0700

    cell01 all

---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+def name() :
+    first_name = "jie"
+    last_name = "varaporn"
+    whole_name = f"{first_name} {last_name}"
+    print(whole_name)
+name()
---
+++ cell01/ex02/age.py
@@ -0,0 +1,6 @@
+def age() :
+    myage = 19
+    bkk = 42
+    sumAge = myage + bkk
+    print(sumAge)
+age()
