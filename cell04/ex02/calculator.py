b7c21adffb00d802425435c9198ee5fbca144dd9
Varaporn Sukmuang <vsukmuan@z2t10c4.42bangkok.com>
2025-05-07 13:37:47 +0700

    fix cell04 ex02

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -5,14 +5,9 @@ def main():
     n2 = int(input("Give me the second number: "))
     print("Thank you!")
 
-    x = n1/n2
-
-    if not x%1 : 
-        x = int(x)
-    else : x = f"{x:.2f}"
 
     print(f"{n1} + {n2} = {n1+n2}")
     print(f"{n1} - {n2} = {n1-n2}")
-    print(f"{n1} / {n2} = {x}")
+    print(f"{n1} / {n2} = {int(n1/n2)}")
     print(f"{n1} * {n2} = {n1*n2}")
 main()
